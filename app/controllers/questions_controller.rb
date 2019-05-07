e50d961c273507a537d01b5f5812f1440a98736c
KoltsovAA <koltsov.alex89@yandex.ru>
2019-05-07 13:54:01 +0300

    code_style

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,7 +8,6 @@ class QuestionsController < ApplicationController
 
   def create
     @question = Question.new(question_params)
-
     if @question.save
       redirect_to user_path(@question.user), notice: 'Вопрос задан'
     else
@@ -26,9 +25,7 @@ class QuestionsController < ApplicationController
 
   def destroy
     user = @question.user
-
-    # Удаляем вопрос
-    @question.destroy
+     @question.destroy
     redirect_to user_path(user), notice: 'Вопрос удален'
   end
 
